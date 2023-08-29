ef34e95a60168d9f9f2d2b661d7d83db6f88a4d3
Usuario <Usuario@DESKTOP-U4P8J7O>
2023-08-28 20:16:37 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Primero: Declaramos nuestras variables
+            float c=0.0f, f=0.0f;
+            Console.Write("ingrese la temperatura en grados Fahrenheint: ");
+            f = Convert.ToSingle(Console.ReadLine()); //Capturamos el dato ingresado por consola y lo convertimos en float 
+            c = (f - 32) * 5 / 9; //Realizamos el calculo de la comversion de los grados Fahrenheint en grados Celcius
+            Console.WriteLine("La temperatura equivalente de {0} grados Fahrenheint en Celcius es: {1}",f,c);//Imprimimos en pantalla los resultados 
+            Console.ReadKey();
+
+
+        }
+    }
+}
